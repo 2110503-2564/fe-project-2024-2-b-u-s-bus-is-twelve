84200081344ab8713d214f4ab1e286fca4d9209d
“maimermaid” <prueksachart254949@gmail.com>
2025-03-25 18:31:26 +0700

    More additional

--- src/components/MyBooking/Banner3.tsx
+++ src/components/MyBooking/Banner3.tsx
@@ -8,18 +8,19 @@ import { useSession } from 'next-auth/react';
 export default function Banner3 () {
 
     return (
-        <div className="block p-1 m-0 w-full h-[60vh] relative" >
-            <Image src='/img/cover5.jpg' 
-                alt='cover'
-                fill={true}
-                priority
-                objectFit="cover"
-            />
-            <div className="relative top-[150px] z-20 text-center text-white">
-            <h1 className="text-center text-4xl md:text-4xl lg:text-5xl font-bold leading-tight ml-20 mt-5 font-playfair">
-                View and Manage Your Bookings Here!
-            </h1>
-            </div>
-        </div>
+       <div className="relative h-[300px] w-full">
+               <Image
+                   src="/img/cover7.jpg"
+                   alt="cover"
+                   fill
+                   priority
+                   className="object-cover"
+               />
+               <div className="absolute top-[100px] w-full text-center text-white z-20">
+                   <h1 className="text-4xl md:text-4xl lg:text-5xl font-bold leading-tight font-playfair">
+                   View and Manage Your Bookings Here!
+                   </h1>
+               </div>
+               </div>
     );
 }
